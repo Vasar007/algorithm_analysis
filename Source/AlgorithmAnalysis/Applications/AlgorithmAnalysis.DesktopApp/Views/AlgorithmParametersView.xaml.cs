066dfcce156a50fefd2b1b0eb6dfbf958d9dabec
Vasily Vasilyev <vasar007@yandex.ru>
2020-03-26 22:57:35 +0300

    Simplify proxy logic for algorithm parameters view

--- Source/AlgorithmAnalysis/Applications/AlgorithmAnalysis.DesktopApp/Views/AlgorithmParametersView.xaml.cs
+++ Source/AlgorithmAnalysis/Applications/AlgorithmAnalysis.DesktopApp/Views/AlgorithmParametersView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace AlgorithmAnalysis.DesktopApp.Views
 {
@@ -8,111 +7,19 @@ namespace AlgorithmAnalysis.DesktopApp.Views
     /// </summary>
     public sealed partial class AlgorithmParametersView : UserControl
     {
-        #region AlgorithmComboBox
+        #region Proxy Properties
 
-        public static readonly DependencyProperty AlgorithmComboBoxProxyProperty =
-            DependencyProperty.Register(
-                nameof(AlgorithmComboBoxProxy),
-                typeof(ComboBox),
-                typeof(AlgorithmParametersView),
-                new PropertyMetadata(default(ComboBox))
-            );
+        public ComboBox AlgorithmComboBoxProxy => AlgorithmComboBox;
 
-        public ComboBox AlgorithmComboBoxProxy
-        {
-            get => (ComboBox) GetValue(AlgorithmComboBoxProxyProperty);
-            set => SetValue(AlgorithmComboBoxProxyProperty, value);
-        }
+        public TextBox FirstValueTextBoxProxy => FirstValueTextBox;
 
-        #endregion
+        public TextBox LastValueTextBoxProxy => LastValueTextBox;
 
-        #region FirstValueTextBox
+        public TextBox LastExtrapolationValueTextBoxProxy => LastExtrapolationValueTextBox;
 
-        public static readonly DependencyProperty FirstValueTextBoxProxyProperty =
-            DependencyProperty.Register(
-                nameof(FirstValueTextBoxProxy),
-                typeof(TextBox),
-                typeof(AlgorithmParametersView),
-                new PropertyMetadata(default(TextBox))
-            );
+        public TextBox LaunchesNumberTextBoxProxy => LaunchesNumberTextBox;
 
-        public TextBox FirstValueTextBoxProxy
-        {
-            get => (TextBox) GetValue(FirstValueTextBoxProxyProperty);
-            set => SetValue(FirstValueTextBoxProxyProperty, value);
-        }
-
-        #endregion
-
-        #region LastValueTextBox
-
-        public static readonly DependencyProperty LastValueTextBoxProxyProperty =
-            DependencyProperty.Register(
-                nameof(LastValueTextBoxProxy),
-                typeof(TextBox),
-                typeof(AlgorithmParametersView),
-                new PropertyMetadata(default(TextBox))
-            );
-
-        public TextBox LastValueTextBoxProxy
-        {
-            get => (TextBox) GetValue(LastValueTextBoxProxyProperty);
-            set => SetValue(LastValueTextBoxProxyProperty, value);
-        }
-
-        #endregion
-
-        #region LastExtrapolationValueTextBox
-
-        public static readonly DependencyProperty LastExtrapolationValueTextBoxProxyProperty =
-            DependencyProperty.Register(
-                nameof(LastExtrapolationValueTextBoxProxy),
-                typeof(TextBox),
-                typeof(AlgorithmParametersView),
-                new PropertyMetadata(default(TextBox))
-            );
-
-        public TextBox LastExtrapolationValueTextBoxProxy
-        {
-            get => (TextBox) GetValue(LastExtrapolationValueTextBoxProxyProperty);
-            set => SetValue(LastExtrapolationValueTextBoxProxyProperty, value);
-        }
-
-        #endregion
-
-        #region LaunchesNumberTextBox
-
-        public static readonly DependencyProperty LaunchesNumberTextBoxProxyProperty =
-            DependencyProperty.Register(
-                nameof(LaunchesNumberTextBoxProxy),
-                typeof(TextBox),
-                typeof(AlgorithmParametersView),
-                new PropertyMetadata(default(TextBox))
-            );
-
-        public TextBox LaunchesNumberTextBoxProxy
-        {
-            get => (TextBox) GetValue(LaunchesNumberTextBoxProxyProperty);
-            set => SetValue(LaunchesNumberTextBoxProxyProperty, value);
-        }
-
-        #endregion
-
-        #region StepValueTextBox
-
-        public static readonly DependencyProperty StepValueTextBoxProxyProperty =
-            DependencyProperty.Register(
-                nameof(StepValueTextBoxProxy),
-                typeof(TextBox),
-                typeof(AlgorithmParametersView),
-                new PropertyMetadata(default(TextBox))
-            );
-
-        public TextBox StepValueTextBoxProxy
-        {
-            get => (TextBox) GetValue(StepValueTextBoxProxyProperty);
-            set => SetValue(StepValueTextBoxProxyProperty, value);
-        }
+        public TextBox StepValueTextBoxProxy => StepValueTextBox;
 
         #endregion
 
@@ -120,13 +27,6 @@ namespace AlgorithmAnalysis.DesktopApp.Views
         public AlgorithmParametersView()
         {
             InitializeComponent();
-
-            AlgorithmComboBoxProxy = AlgorithmComboBox;
-            FirstValueTextBoxProxy = FirstValueTextBox;
-            LastValueTextBoxProxy = LastValueTextBox;
-            LastExtrapolationValueTextBoxProxy = LastExtrapolationValueTextBox;
-            LaunchesNumberTextBoxProxy = LaunchesNumberTextBox;
-            StepValueTextBoxProxy = StepValueTextBox;
         }
     }
 }
