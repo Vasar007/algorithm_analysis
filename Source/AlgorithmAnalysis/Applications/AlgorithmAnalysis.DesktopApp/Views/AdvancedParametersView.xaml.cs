ca1b1002e185ea4c42c1ba76222e1d16267addee
Vasily Vasilyev <vasar007@yandex.ru>
2020-03-26 23:37:01 +0300

    Add advanced parameters view

---
+++ Source/AlgorithmAnalysis/Applications/AlgorithmAnalysis.DesktopApp/Views/AdvancedParametersView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AlgorithmAnalysis.DesktopApp.Views
+{
+    /// <summary>
+    /// Interaction logic for AdvancedParametersView.xaml
+    /// </summary>
+    public sealed partial class AdvancedParametersView : UserControl
+    {
+        public AdvancedParametersView()
+        {
+            InitializeComponent();
+        }
+    }
+}
