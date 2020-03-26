9fa2c8e7d7b6f271b7c1c87c1e3041372c964f0b
Vasily Vasilyev <vasar007@yandex.ru>
2020-03-26 23:17:29 +0300

    Add analysis parameters view

---
+++ Source/AlgorithmAnalysis/Applications/AlgorithmAnalysis.DesktopApp/Views/AnalysisParametersView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AlgorithmAnalysis.DesktopApp.Views
+{
+    /// <summary>
+    /// Interaction logic for AnalysisParametersView.xaml
+    /// </summary>
+    public sealed partial class AnalysisParametersView : UserControl
+    {
+        public AnalysisParametersView()
+        {
+            InitializeComponent();
+        }
+    }
+}
